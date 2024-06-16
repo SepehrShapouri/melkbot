41394181c3c0bada0b23a43b06e58e60a6cb7037
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-06-16 15:49:27 +0330

    feat:first commit

---
+++ app/not-available/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>the app is only accessible through mobile</div>;
+}
+
+export default page;
---
+++ middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const userAgent = request.headers.get("user-agent") || "";
+  const isMobile = /mobile/i.test(userAgent);
+  const url = request.nextUrl.clone();
+
+  if (!isMobile && !url.pathname.startsWith("/not-available")) {
+    url.pathname = "/not-available";
+    return NextResponse.redirect(url);
+  }
+
+  if (isMobile && url.pathname.startsWith("/not-available")) {
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|favicon.ico).*)"],
+};
