efba66e33e69d4dfcb2ec02e655cfedac6ed27e7
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-07-17 11:32:19 +0330

    style:chanegd offers styles

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -148,35 +148,45 @@ function Home() {
           </div>
           <div className="flex-1 flex flex-col px-6 pb-10 overflow-auto no-scrollbar">
             <div class="flex justify-between items-center gap-4 mb-6">
-              <p class="font-bold text-gray-800 dark:text-white">درخواست های اخیر</p>
-              <a class="text-sm text-gray-500 dark:text-gray-200" href="/expenses">
+              <p class="font-bold text-gray-800 dark:text-white">
+                درخواست های اخیر
+              </p>
+              <a
+                class="text-sm text-gray-500 dark:text-gray-200"
+                href="/expenses"
+              >
                 مشاهده همه
               </a>
             </div>
-            <ScrollArea className="
-            mb-[2rem]
-        h-[370px] ">
-            <div className="flex flex-col gap-[0.5rem] ">
-              {offers.map((offer,index) => {
-                return (
-                  <div className="w-full bg-white dark:bg-slate-900  shadow-md py-[1rem] px-[1rem] rounded-md" key={index}>
-                    <div className="flex items-center gap-[0.5rem]">
-                      <div className="aspect-square w-[45px] h-[45px] rounded-lg flex items-center justify-center cursor-pointer bg-blue-200 ">
-                        {offer.property_type == "land" ? (
-                          <Landmark className="text-blue-600" />
-                        ) : (
-                          <Building className="text-blue-600" />
-                        )}
+            <ScrollArea
+              className="
+            mb-[2rem] py-4
+        h-[350px] "
+            >
+              <div className="flex flex-col gap-[0.5rem] ">
+                {offers.map((offer, index) => {
+                  return (
+                    <div
+                      className="w-full bg-white dark:bg-slate-900  shadow-md py-[1rem] px-[1rem] rounded-md"
+                      key={index}
+                    >
+                      <div className="flex items-center gap-[0.5rem]">
+                        <div className="aspect-square w-[50px] h-[50px] rounded-lg flex items-center justify-center cursor-pointer bg-[#C0DBFE] dark:bg-[#263A8A] ">
+                          {offer.property_type == "land" ? (
+                            <Landmark className=" text-[#1D4ED8] dark:text-[#93C5FD]" />
+                          ) : (
+                            <Building className=" text-[#1D4ED8] dark:text-[#93C5FD]" />
+                          )}
+                        </div>
+                        <span>
+                          <h2>{offer.title}</h2>
+                          <p>{offer.desc}</p>
+                        </span>
                       </div>
-                      <span>
-                        <h2>{offer.title}</h2>
-                        <p>{offer.desc}</p>
-                      </span>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
+                  );
+                })}
+              </div>
             </ScrollArea>
           </div>
         </div>
